214745bb0da22c0e655700be51f1adfca2c7df69
VladimirShinkevich <vladimirshinkevich1988@mail.ru>
2021-04-11 16:29:32 +0300

    В методах joins аргументом используется SQL

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,5 +1,5 @@
 class Test < ApplicationRecord
   def self.tests_by_categories(category_title)
-    joins(:categories).where(categories: { title: category_title }).order(id: :desc).pluck(:tests)
+    joins("JOIN categories ON tests.category_id = categories.id").where(categories: { title: category_title }).order(id: :desc).pluck(:title)
   end	
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  def tests_by_level(level)
-    Test.joins(:pass_tests).where(pass_tests: { user_id: id, level: level })
+  def tests_by_level(level)  
+    Test.joins("JOIN pass_tests ON tests.id = pass_tests.test_id").where("pass_tests.user_id = :user_id AND level = :level", { user_id: id, level: level })
   end
 end
