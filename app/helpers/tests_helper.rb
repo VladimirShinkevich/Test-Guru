f120c2b4bdd798b49b47dc2abbfdd6bc62555927
VladimirShinkevich <vladimirshinkevich1988@mail.ru>
2021-04-26 21:56:58 +0300

    Реализованы все стандартные действия вместе с представлениями (видами) для ресурса вопросов. Добавлен футер в котором выводиться текущий год, имя автора, название проекта (в виде ссылки на GitHub-репозиторий проекта), его краткое описание и ссылка ведущая на адрес tihnknetica.com. Написаны хэлперы: для заголовка формы создания и редактирования вопроса, для вывода текущего года в футере, для формирования ссылки на GitHub репозиторий: github_url(author, repo). Добавлено в представление списка тестов количество вопросов для каждого теста.

--- app/helpers/tests_helper.rb
+++ app/helpers/tests_helper.rb
@@ -1,2 +1,8 @@
 module TestsHelper
+  
+  TEST_LEVEL = { 0 => :easy, 1 => :easy, 2 => :normal, 3 => :normal, 4 => :advanced, 5 => :advanced, 6 => :hard }.freeze
+
+  def test_level(test)
+    TEST_LEVEL[test.level] || :professional
+  end
 end
