bd5fe660ff78d82a17eaf319ac72951fc91536ff
VladimirShinkevich <vladimirshinkevich1988@mail.ru>
2021-05-13 21:09:49 +0300

    Стилистическое оформление кода.

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,55 +1,55 @@
 class QuestionsController < ApplicationController
-	before_action :find_question, only: %i[show edit update destroy]
-	before_action :find_test, only: %i[new create]
-	rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_find
+  before_action :find_question, only: %i[show edit update destroy]
+  before_action :find_test, only: %i[new create]
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_find
 
-	def show; end
+  def show; end
 
-	def new
-		@question = @test.questions.new
-	end 
+  def new
+    @question = @test.questions.new
+  end 
 
-	def create
-		@question = @test.questions.new(question_params)
-		if @question.save
-			redirect_to @question
-		else
-			render :new
-		end   
-	end
+  def create
+    @question = @test.questions.new(question_params)
+    if @question.save
+      redirect_to @question
+    else
+      render :new
+    end   
+  end
 
-	def edit
-		@test = @question.test
-	end
+  def edit
+    @test = @question.test
+  end
 
-	def update
-		if @question.update(question_params)
-			redirect_to @question
-		else
-			render :edit
-		end  
-	end
+  def update
+    if @question.update(question_params)
+      redirect_to @question
+    else
+      render :edit
+    end  
+  end
 
-	def destroy
-		@question.destroy
-		redirect_to tests_path(@question.test)
-	end
+  def destroy
+    @question.destroy
+    redirect_to tests_path(@question.test)
+  end
 
-	private
+  private
 
-	def question_params
-		params.require(:question).permit(:body)
-	end
+  def question_params
+    params.require(:question).permit(:body)
+  end
 
-	def find_question
-		@question = Question.find(params[:id])
-	end
+  def find_question
+    @question = Question.find(params[:id])
+  end
 
-	def find_test
-		@test = Test.find(params[:test_id])
-	end
+  def find_test
+    @test = Test.find(params[:test_id])
+  end
 
-	def rescue_with_question_not_find
-		render plain: "Question not found!"
-	end
+  def rescue_with_question_not_find
+    render plain: "Question not found!"
+  end
 end
