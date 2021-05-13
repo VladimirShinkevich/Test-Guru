35c74e5270941756fe2107605683377546f6e756
VladimirShinkevich <vladimirshinkevich1988@mail.ru>
2021-05-13 21:24:25 +0300

    Стилистическое оформление кода.

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,21 @@
 Rails.application.routes.draw do
-	
-	root 'tests#index'
+  
+  root 'tests#index'
 
-	resources :tests do
-		resources :questions, shallow: true, expend: :index do
-			resources :answers, shallow: true, expend: :index
-		end
-	 
-		member do
-			post :start
-		end
-	end 
+  resources :tests do
+    resources :questions, shallow: true, expend: :index do
+      resources :answers, shallow: true, expend: :index
+    end
+   
+    member do
+      post :start
+    end
+  end 
  
-	resources :test_passages, only: %i[show update] do
-		member do
-			get :result
-		end
-	end
+  resources :test_passages, only: %i[show update] do
+    member do
+      get :result
+    end
+  end
 
 end
