0ac05edb57a111f17ea9f1e4879dede389bf2f53
VladimirShinkevich <vladimirshinkevich1988@mail.ru>
2021-05-25 19:11:02 +0300

    При регистрации пользователя выводиться флэш-сообщение.

--- app/controllers/admin/tests_controller.rb
+++ app/controllers/admin/tests_controller.rb
@@ -14,7 +14,7 @@ class Admin::TestsController < Admin::BaseController
   end
 
   def create
-    @test = Test.new(test_params)
+    @test = current_user.authors_tests.new(test_params)
     if @test.save
       redirect_to [:admin, @test]
     else
@@ -35,11 +35,6 @@ class Admin::TestsController < Admin::BaseController
   def destroy
     @test.destroy
     redirect_to admin_tests_path
-  end
-
-  def start
-    current_user.tests.push(@test)
-    redirect_to current_user.test_passage(@test)
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@
 Rails.application.routes.draw do
   root 'tests#index'
 
-  devise_for :users, path: :gurus, path_names: { sing_in: :login, sing_out: :logout }
+  devise_for :users, path: :gurus, path_names: { sing_in: :login, sing_out: :logout }, controllers: { sessions: "users/sessions" }
 
   resources :tests, only: :index do
     member do
