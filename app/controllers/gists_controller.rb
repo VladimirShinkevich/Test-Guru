8a6e24b79bcacef233f5c6161d6526dd465a9b64
VladimirShinkevich <vladimirshinkevich1988@mail.ru>
2021-06-14 21:28:52 +0300

    Работа со стилем кода

--- app/controllers/gists_controller.rb
+++ app/controllers/gists_controller.rb
@@ -1,14 +1,14 @@
 class GistsController < ApplicationController
-	def create
-	  @test_passage = TestPassage.find(params[:id])
-	  result = GistQuestionService.new(@test_passage.current_question).call
+  def create
+    @test_passage = TestPassage.find(params[:id])
+    result = GistQuestionService.new(@test_passage.current_question).call
 
-	  if result.success?
-	    flash[:notice] = "#{t(".success")} #{view_context.link_to(t(".find_here"), result.html_url, target: "_blank")}".html_safe
-	     Gist.create(url: result.html_url, user: current_user, question: @test_passage.current_question)
-	  else
-	    flash[:notice] = t(".unsuccess")
-	  end
-	  redirect_to @test_passage
-	end
+    if result.success?
+      flash[:notice] = "#{t(".success")} #{view_context.link_to(t(".find_here"), result.html_url, target: "_blank")}".html_safe
+      Gist.create(url: result.html_url, user: current_user, question: @test_passage.current_question)
+    else
+      flash[:notice] = t(".unsuccess")
+    end
+  redirect_to @test_passage
+  end
 end
